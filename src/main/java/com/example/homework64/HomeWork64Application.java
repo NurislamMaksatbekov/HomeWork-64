eab7760d895771b36645da6a339a220b5ad906d3
Nurislam <maksatbekkov@icloud.com>
2023-04-28 19:29:46 +0600

    Lab Work started

---
+++ src/main/java/com/example/homework64/HomeWork64Application.java
@@ -0,0 +1,13 @@
+package com.example.homework64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork64Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork64Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework64/HomeWork64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork64ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
