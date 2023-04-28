6f9aceb93e170986af7d9d9181acd8bf70d6f711
Nurislam <maksatbekkov@icloud.com>
2023-04-28 20:19:34 +0600

    added function addTask

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,1 +1,9 @@
-''
+function addTask(task) {
+    const tasks = document.createElement("li");
+    tasks.innerHTML=task;
+    tasks.addEventListener("dblclick",(e)=> {
+        e.target.classList.add("line");
+    });
+    const listEvent = document.getElementById('list-event')
+    listEvent.appendChild(tasks)
+}
